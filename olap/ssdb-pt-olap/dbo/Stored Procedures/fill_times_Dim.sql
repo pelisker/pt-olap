170426b39cd1d9e5708b6007bdc20f55f8de5fac
Ivan K <ww_dementor@mail.ru>
2016-09-11 23:58:07 +0300

    Сделал два конфига для быстрого переключения между коннектами к базе данных. Моя DevKID. Твоя назвал DevPelisker (вырвал название из аккаунта гита) Переименовал все на русский. Сделал иерархии, поправил таблицу время и сделал в ней иерархии.

--- olap/ssdb-pt-olap/dbo/Stored Procedures/fill_times_Dim.sql
+++ olap/ssdb-pt-olap/dbo/Stored Procedures/fill_times_Dim.sql
@@ -4,28 +4,32 @@ BEGIN
 
 	truncate table t_times_dim
 
-	insert into t_times_dim (id, [date], [dateName], [day],[week],[month],[monthnum],[dayofweek],[dayofweeknum],[year])
+	insert into t_times_dim (id, [date], [dateName], [day],[week],[weekNum],[month],[monthnum],[dayofweek],[dayofweeknum],[year])
 	select 
 		   id=datepart(YEAR,CONVERT(datetime,День))*10000 + datepart(M,CONVERT(datetime,День))*100 + datepart(D,CONVERT(datetime,День)),
 		   [date] = convert(varchar(10),CONVERT(datetime,День),101),
 		   [datename]= convert(varchar(8),CONVERT(datetime,День),4),
 		   [day] = День,
-		   Неделя = datepart(WEEK,CONVERT(datetime,День)),
+		   Неделя = 'Неделя ' + cast(datepart(WEEK,CONVERT(datetime,День)) as varchar(10)),
+		   Неделя2sort = 
+		   cast(datepart(yy,CONVERT(datetime,День)) as varchar(10)) + '_' 
+		   + cast(FORMAT(datepart(MM,CONVERT(datetime,День)) , '00','en-US')as varchar(2)) + '_' 
+		   + cast(FORMAT(datepart(WEEK,CONVERT(datetime,День)) , '00','en-US')as varchar(2)),
 		   case datepart(mm,CONVERT(datetime,День))
-			 when 1 then 'январь'
-			 when 2 then 'февраль'
-			 when 3 then 'март'
-			 when 4 then 'апрель'
-			 when 5 then 'май'
-			 when 6 then 'июнь'
-			 when 7 then 'июль'
-			 when 8 then 'август'
-			 when 9 then 'сентябрь'
-			 when 10 then 'октябрь'
-			 when 11 then 'ноябрь'
-			 when 12 then 'декабрь'
+			 when 1 then 'Январь'
+			 when 2 then 'Февраль'
+			 when 3 then 'Март'
+			 when 4 then 'Апрель'
+			 when 5 then 'Май'
+			 when 6 then 'Июнь'
+			 when 7 then 'Июль'
+			 when 8 then 'Август'
+			 when 9 then 'Сентябрь'
+			 when 10 then 'Октябрь'
+			 when 11 then 'Ноябрь'
+			 when 12 then 'Декабрь'
 		   end Месяц,
-		   МесяцДляСортировки = datepart(mm,CONVERT(datetime,День)),
+			cast(datepart(yy,CONVERT(datetime,День)) as varchar(4)) + '-' + cast(FORMAT(datepart(MM,CONVERT(datetime,День)) , '00','en-US')as varchar(2)) Месяц2sort,
 		   case lower(datename(weekday,CONVERT(datetime,День)))
 			 when 'monday' THEN 'понедельник'
 			 when 'tuesday' THEN 'вторник'
@@ -35,20 +39,24 @@ BEGIN
 			 when 'saturday' THEN 'суббота'
 			 when 'sunday' THEN 'воскресенье'
 		   end AS ДеньНедели,
-		   case lower(datename(weekday,CONVERT(datetime,День)))
-			 when 'monday' THEN 1
-			 when 'tuesday' THEN 2
-			 when 'wednesday' THEN 3
-			 when 'thursday' THEN 4
-			 when 'friday' THEN 5
-			 when 'saturday' THEN 6
-			 when 'sunday' THEN 7
-		   end as ДеньНеделиДляСортировки,
+		   cast(datepart(yy,CONVERT(datetime,День)) as varchar(10)) + '_' 
+		   + cast(FORMAT(datepart(MM,CONVERT(datetime,День)) , '00','en-US')as varchar(2)) + '_' 		   
+		   + cast(FORMAT(datepart(WEEK,CONVERT(datetime,День)) , '00','en-US')as varchar(2)) + '_' 
+		   + case datename(weekday,CONVERT(datetime,День))
+			 when 'monday' THEN '01'
+			 when 'tuesday' THEN '02'
+			 when 'wednesday' THEN '03'
+			 when 'thursday' THEN '04'
+			 when 'friday' THEN '05'
+			 when 'saturday' THEN '06'
+			 when 'sunday' THEN '07'
+		   end 
+		   as ДеньНеделиДляСортировки,
 		   datepart(yy,CONVERT(datetime,День)) as Год
 
 	  from (-- дни
 			select 40177 + (ROW_NUMBER() over(ORDER BY v1.number)-1) as День from master.dbo.spt_values v1 full join (SELECT code=1 UNION SELECT code=2 UNION SELECT code=3) v2 on 1=1
 		   ) as t1
 	  where CONVERT(datetime,День) >= '20140101' and
-			CONVERT(datetime,День) <= getdate()+100       
+			CONVERT(datetime,День) <= getdate()+100
 END
--- olap/ssdb-pt-olap/dbo/Tables/t_times_dim.sql
+++ olap/ssdb-pt-olap/dbo/Tables/t_times_dim.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE [dbo].[t_times_dim] (
-    [id]           INT          NOT NULL,
-    [date]         DATE         NOT NULL,
-    [day]          INT          NULL,
-    [week]         INT          NULL,
-    [month]        VARCHAR (50) NULL,
-    [monthNum]     INT          NULL,
-    [DayOfWeek]    VARCHAR (50) NULL,
-    [DayOfWeekNum] INT          NULL,
-    [year]         INT          NULL,
-    [dateName]     VARCHAR (10) NULL,
+    [id]           INT           NOT NULL,
+    [date]         DATE          NOT NULL,
+    [day]          INT           NULL,
+    [week]         VARCHAR (50)  NULL,
+    [weekNum]      VARCHAR (50)  NULL,
+    [month]        VARCHAR (50)  NULL,
+    [monthNum]     NVARCHAR (50) NULL,
+    [DayOfWeek]    VARCHAR (50)  NULL,
+    [DayOfWeekNum] NVARCHAR (50) NULL,
+    [year]         INT           NULL,
+    [dateName]     VARCHAR (10)  NULL,
     CONSTRAINT [PK_times_dim] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
